7842d07faeb08a4fcf99646f80bc7945f14661d0
Neil Greatorex <neil@greatorex.me.uk>
2026-08-02 21:11:06 +0100

    Logging improvements

--- run.sh
+++ run.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 # Setup logging
-truncate -s 0 build.log
-exec > >(tee build.log) 2>&1
+exec > >(tee --append build.log) 2>&1
 
 BRANCH=`git rev-parse --abbrev-ref HEAD`
-TIME=`date "+%Y-%m-%d %H:%M:%S"`
 
+TIME=`date "+%Y-%m-%d %H:%M:%S"`
 echo "[${TIME}] Running pi-gen for branch ${BRANCH}..."
 
 # Execute pi-gen
 sudo ./build.sh
 
 # Generate manifest from deploy directory
+TIME=`date "+%Y-%m-%d %H:%M:%S"`
+echo "[${TIME}] Updating Pi Imager manifest..."
+
 python manifest/add_custom_images.py
 
