2d3654bc7e0e7698237b5c06a9e5194633b4918b
Neil Greatorex <neil@greatorex.me.uk>
2026-08-02 19:47:56 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-echo > build.log
 
-sudo ./build.sh 2>&1 | tee build.log
-python manifest/add_custom_images.py | tee build.log
+# Setup logging
+truncate -s 0 build.log
+exec > >(tee build.log) 2>&1
 
-#pushd deploy/
-#tar -zcvf custom-images.tar.gz *
-#popd
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+TIME=`date "+%Y-%m-%d %H:%M:%S"`
+
+echo "[${TIME}] Running pi-gen for branch ${BRANCH}..."
+
+# Execute pi-gen
+sudo ./build.sh
+
+# Generate manifest from deploy directory
+python manifest/add_custom_images.py
 
